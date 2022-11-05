971baf57c634176475fbba89fe0e2798413f43dd
WIROONYAPAT NAOSAWAT <wiroonyapat_n@cmu.ac.th>
2022-11-06 02:53:27 +0700

    ImportHook Conflict เป็นล้านแน่นอน ระวังจะไม่ได้นอนนะ

---
+++ OnceTwoTree_game1/Content/TileManagement/Tileset_Decorate_TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset_Decorate" tilewidth="108" tileheight="108" tilecount="12" columns="4">
+ <image source="../Resources/TileSet/Tileset_Decorate.png" width="513" height="405"/>
+</tileset>
--- OnceTwoTree_game1/PlayerOne.cs
+++ OnceTwoTree_game1/PlayerOne.cs
@@ -11,14 +11,14 @@ using MonoGame.Extended.Collisions;
 
 namespace OnceTwoTree_game1
 {
-    public class PlayerOne : IEntity
+    public class PlayerOne : Player 
     {
         private readonly Game1 _game;
 
         public int Velocity = 10;
         Vector2 move;
 
-        public IShapeF Bounds { get; }
+        public override IShapeF Bounds { get; }
 
         protected Texture2D P_idle,P_climb,P_walk,P_fall;
         protected Texture2D UI_stamina;
@@ -51,7 +51,6 @@ namespace OnceTwoTree_game1
         float totalTime;
         float maxTime;
         #endregion
-
 
 
         internal HookBox myhook;
@@ -129,15 +128,13 @@ namespace OnceTwoTree_game1
             isFlip = false;
             leftHand = false;
             onAir = false;
-            
 
-
-            HookBox.LoadHookToPlayer(this, this._game);
+            myhook = HookBox.LoadHookToPlayer(this, this._game);
            
         }
 
         
-        public virtual void Update(GameTime gameTime)
+        public override void Update(GameTime gameTime)
         {
             PlayerControl(gameTime);
 
@@ -209,7 +206,7 @@ namespace OnceTwoTree_game1
             
         }
 
-        public virtual void Draw(SpriteBatch spriteBatch)
+        public override void Draw(SpriteBatch spriteBatch)
         {
             if (!isFlip)
             {
@@ -279,7 +276,7 @@ namespace OnceTwoTree_game1
             this.myhook.Draw(spriteBatch);
         }
         
-        public void OnCollision(CollisionEventArgs collisionInfo)
+        public override void OnCollision(CollisionEventArgs collisionInfo)
         {
             countC++;
             //Platform Collision
@@ -674,181 +671,5 @@ namespace OnceTwoTree_game1
         #endregion
     }
 
-    internal class HookBox : IEntity
-    {
-
-        private readonly Game1 _game;
-
-        public IShapeF Bounds { get; }
-
-        Texture2D _RopeTexture; // รอimportรูปเชือก
-        Texture2D _RopeTexture_Test;
-
-        bool _ishooking = false;
-        public double _myTime = 0.0;
-
-        PlayerOne myPlayer;
-
-        static List<PlayerOne> _PlayerControl = new List<PlayerOne>();
-        public static List<HookBox> _Hookboxes = new List<HookBox>();
-
-        CollisionComponent _myBox;
-
-
-        internal HookBox(Game1 game, IShapeF myShape)
-        {
-            this._game = game;
-            Bounds = myShape;
-
-            //สำหรับ Test
-            _RopeTexture_Test = new Texture2D(game.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-            _RopeTexture_Test.SetData(new[] { Color.Aqua });
-
-            //สำหรับ Load Texture เชือก
-            //_RopeTexture = _game.Content.Load<Texture2D>();
-
-        }
-
-
-        public static void LoadHookToPlayer(PlayerOne _mPlayer, Game1 game)
-        {
-            _PlayerControl.Add(_mPlayer);
-            _Hookboxes.Add(new HookBox(game, new RectangleF(Point2.Zero, new Size2(108, 108))));
-            _mPlayer.myhook = _Hookboxes[_Hookboxes.Count-1];
-        }
-
-
-        public enum throwing_state { throwing, startthrow, finalthrow, idle }
-
-        public throwing_state throw_state = throwing_state.idle;
-
-        KeyboardState _oldkey;
-        public virtual void Update(GameTime gameTime)
-        {
-            KeyboardState keyboard = Keyboard.GetState();
-
-            #region ThrowingPlayer1
-            if ((keyboard.IsKeyDown(Keys.E) && (_Hookboxes[0].throw_state == throwing_state.idle)) || _Hookboxes[0].throw_state == throwing_state.startthrow) // สำหรับ player 1 แม้จะมี player แค่คนเดียว
-            {
-                if (_Hookboxes[0].throw_state != throwing_state.throwing)
-                {
-                    //อาจจะ set Animate ไว้ตรงนี้ก็ได้
-                }
-
-                if (_oldkey.IsKeyUp(Keys.E) && (_Hookboxes[0].throw_state == throwing_state.idle))
-                {
-                    myPlayer = _PlayerControl[0];
-                    _Hookboxes[0].throw_state = throwing_state.startthrow;
-                    _Hookboxes[0].ThrowHook(gameTime);
-                }
-
-                else if (_Hookboxes[0].throw_state == throwing_state.startthrow)
-                {
-                    _Hookboxes[0].throw_state = throwing_state.throwing;
-                    _Hookboxes[0].ThrowHook(gameTime);
-                }
-                
-            }
-
-            else if (_Hookboxes[0].throw_state == throwing_state.throwing)
-            {
-                _Hookboxes[0].ThrowHook(gameTime);
-            }
-
-            else if (_Hookboxes[0].throw_state == throwing_state.finalthrow)
-            {
-                _Hookboxes[0].ThrowHook(gameTime);
-                throw_state = throwing_state.idle;
-            }
-            #endregion
-                        
-            _oldkey = keyboard;
-        }
-                
-
-        public virtual void Draw(SpriteBatch spriteBatch)
-        {
-            if (this.throw_state == throwing_state.idle)
-            {
-                
-            }
-            else
-            {
-                spriteBatch.DrawRectangle((RectangleF)Bounds, Color.Yellow, 3f);
-
-                double distancerope = Vector2.Distance(myPlayer.Bounds.Position, this.Bounds.Position);
-                float ropeThickness = 5;
-                float ropeJoint = 10;
-
-                for(int i = 0; i < (int)(distancerope); i += (int)ropeJoint)
-                {
-                    Rectangle Start = new Rectangle((int)(myPlayer.Bounds.Position.X + ((108 + ropeThickness) * 0.5)), (int)(myPlayer.Bounds.Position.Y + (i * Math.Sin( - Math.PI/2.0)) + ((108 + ropeThickness) * 0.5)),(int)ropeJoint, (int)ropeThickness);
-                    spriteBatch.Draw(this._RopeTexture_Test, Start, null, Color.Fuchsia, (float)( Math.PI / 2.0), new Vector2(0, ropeThickness * 0.5f), 0, 0);
-                }
-            }
-        }
-
-        public void OnCollision(CollisionEventArgs collisionInfo)
-        {
-
-        }
-
-        double distance = 108 * 5;
-
-        public void ThrowHook(GameTime _mTimimg)
-        {
-            if (throw_state == throwing_state.startthrow)
-            {
-                this.Bounds.Position = new Vector2(this.myPlayer.Bounds.Position.X, this.myPlayer.Bounds.Position.Y);
-                _myTime += _mTimimg.GetElapsedSeconds();
-            }
-
-            if (throw_state == throwing_state.throwing)
-            {
-                double distance = 0, startValo = 0, time = _myTime, gavity = 3000;
-
-                startValo = - Math.Sqrt(2 * gavity * this.distance);
-                distance = (startValo * time) + ((0.5) * gavity * Math.Pow(time, 2));
-                this.Bounds.Position = new Vector2(this.myPlayer.Bounds.Position.X, this.myPlayer.Bounds.Position.Y + (float)distance);
-
-                if(distance > -10)
-                {
-                    throw_state = throwing_state.finalthrow;
-                }
-
-                _myTime += _mTimimg.GetElapsedSeconds();
-            }
-
-            if (throw_state == throwing_state.finalthrow)
-            {
-                _myTime = 0;
-            }
-        }
-
-        public static void Update_Hook_Hitblock(List<IEntity> _myCollide)
-        {
-            foreach (HookBox x in _Hookboxes)
-            {
-                if (x.throw_state == throwing_state.startthrow)
-                {
-                    _myCollide.Add(x);
-                }
-                else if (x.throw_state == throwing_state.throwing)
-                {
-                    _myCollide.Remove(x);
-                    _myCollide.Add(x);
-                }
-                else if (x.throw_state == throwing_state.finalthrow)
-                {
-                    _myCollide.Remove(x);
-                }
-                else if (x.throw_state == throwing_state.idle)
-                {
-                    _myCollide.Remove(x);
-                }
-            }
-        }
-
-
-    }
+    
 }
